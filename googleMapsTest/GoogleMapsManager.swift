f1d65e1f09c11f282662a023bd6ca90bab70d225
fred <fred@maxkit.com.tw>
2020-03-20 11:27:11 +0800

    init version

---
+++ googleMapsTest/GoogleMapsManager.swift
@@ -0,0 +1,237 @@
+//
+//  GoogleMapsManager.swift
+//  googleMapsTest
+//
+//  Created by maxkitmac on 2020/3/17.
+//  Copyright © 2020年 fredshao. All rights reserved.
+//
+
+import Foundation
+import GoogleMaps
+
+// MARK: - Class Data
+
+class GoogleMapsData {
+    // 幾邊形
+    static var NUM_OF_POLYGON = Int()
+    
+    // 計算多邊形已經建立幾個頂點了
+    static var count = 0
+    
+    // 多邊形所有頂點座標
+    static var polygonPoints = [CLLocationCoordinate2D]()
+    
+    // 多邊形所有頂點標誌的資訊
+    static var polygonMarkers = [GMSMarker]()
+    
+    //
+    static var trackMarkers = [GMSMarker]()
+    
+    // 用來繪製多邊形的物件
+    static var polygonPath = GMSMutablePath()
+    
+    // 用來繪製軌跡的物件
+    static var trackPath = GMSMutablePath()
+    
+    // 測試點
+    static let testMarker = GMSMarker()
+}
+
+// MARK: - Class
+
+class GoogleMapsManager {
+    
+    // MAEK: - Properties
+    
+    static let shareInstance = GoogleMapsManager()
+}
+
+// MARK:- Public functions
+
+extension GoogleMapsManager {
+    func newTestPoint(coordinate: CLLocationCoordinate2D, mapView: GMSMapView) {
+        GoogleMapsData.testMarker.position = coordinate
+        GoogleMapsData.testMarker.isDraggable = false
+        GoogleMapsData.testMarker.title = "Test Point"
+        GoogleMapsData.testMarker.snippet = ""
+        GoogleMapsData.testMarker.map = mapView
+    }
+    
+    // 建立多邊形的每一個頂點
+    func newPoint(coordinate: CLLocationCoordinate2D, forPolygon mapView: GMSMapView) {
+        
+        let marker = GMSMarker()
+        
+        marker.position = coordinate
+        marker.isDraggable = true
+        marker.title = "Point" + "\(GoogleMapsData.count)" // 以 Point1/ Point2/ ... 為鍵值
+        marker.snippet = (GoogleMapsData.count == GoogleMapsData.NUM_OF_POLYGON) ? "Original Point" : ""
+        marker.icon = UIImage(named: "warning-icon")
+        marker.map = mapView
+        
+        GoogleMapsData.polygonPoints.append(coordinate)
+        GoogleMapsData.polygonMarkers.append(marker)
+        
+        GoogleMapsData.polygonPath.add(coordinate)
+        
+        GoogleMapsData.count -= 1
+        
+        if GoogleMapsData.count == 0 {
+            GoogleMapsData.polygonPath.add(GoogleMapsData.polygonPoints[0])
+            
+        }
+        drawPolygon(mapView: mapView)
+    }
+    
+    func newPoints(coordinates: [CLLocationCoordinate2D], forTrack mapView: GMSMapView) {
+        for (index, coordinate) in coordinates.enumerated() {
+            let marker = GMSMarker()
+            
+            marker.position = coordinate
+            marker.isDraggable = false
+            marker.title = "Point" + "\(index)"
+            marker.snippet = ""
+            marker.icon = UIImage(named: "walk-icon")
+            marker.map = mapView
+            
+            GoogleMapsData.trackPath.add(coordinate)
+        }
+        drawTrack(mapView: mapView)
+    }
+    
+    func checkIsInPolygon(coordinate: CLLocationCoordinate2D) -> Bool {
+        let testPoint = CGPoint(x: coordinate.latitude, y: coordinate.longitude)
+        let polygon = GoogleMapsData.polygonPoints.map {
+            (coor: CLLocationCoordinate2D) -> CGPoint in
+            return CGPoint(x: coor.latitude, y: coor.longitude)
+        }
+        return contains(polygon: polygon, test: testPoint)
+    }
+    
+    // 選擇為幾邊形
+    func setNumOfPolygon(num: Int) {
+        GoogleMapsData.count = num
+        GoogleMapsData.NUM_OF_POLYGON = num
+    }
+    
+    // 檢查多邊形是否已繪製完成
+    func checkFinishDrawing() -> Bool {
+        return (GoogleMapsData.count == 0) ? true : false
+    }
+    
+    func removeTestPoint() {
+        GoogleMapsData.testMarker.map = nil
+    }
+
+    func resetMap(mapView: GMSMapView) {
+        mapView.clear()
+        resetDrawingPolygon()
+        resetDrawingTrack()
+    }
+    
+    // 清除多邊形
+    func resetDrawingPolygon() {
+        GoogleMapsData.polygonPoints.removeAll()
+        GoogleMapsData.polygonMarkers.removeAll()
+        
+        GoogleMapsData.polygonPath.removeAllCoordinates()
+        GoogleMapsData.count = GoogleMapsData.NUM_OF_POLYGON
+    }
+    
+    func resetDrawingTrack() {
+        GoogleMapsData.trackPath.removeAllCoordinates()
+    }
+    
+    
+    // 當移動多邊形某頂點時, 更新其位置
+    func modifyPoint(newMarker: GMSMarker, mapView: GMSMapView) {
+        
+        for (index, marker) in GoogleMapsData.polygonMarkers.enumerated() {
+            if marker.title == newMarker.title {
+                GoogleMapsData.polygonMarkers[index].position = newMarker.position
+                GoogleMapsData.polygonPoints[index] = newMarker.position
+                GoogleMapsData.polygonPath.replaceCoordinate(at: UInt(index), with: newMarker.position)
+                
+                // 原點(第一個點)鍵值
+                let originalPoint = "Point" + "\(GoogleMapsData.NUM_OF_POLYGON)"
+                
+                // 如果現在移動的是原點, 把路徑的最後一個點重設為原點
+                if newMarker.title == originalPoint {
+                    GoogleMapsData.polygonPath.replaceCoordinate(at: UInt(GoogleMapsData.NUM_OF_POLYGON), with: GoogleMapsData.polygonPoints[0])
+                }
+            }
+        }
+        reDrawing(mapView: mapView)
+    }
+    
+    // 當移動多邊形某頂點時, 更新其位置
+    func modifyPoint(newMarker: GMSMarker, whenNotFinishDrawingYet mapView: GMSMapView) {
+        
+        for (index, marker) in GoogleMapsData.polygonMarkers.enumerated() {
+            if marker.title == newMarker.title {
+                GoogleMapsData.polygonMarkers[index].position = newMarker.position
+                GoogleMapsData.polygonPoints[index] = newMarker.position
+                GoogleMapsData.polygonPath.replaceCoordinate(at: UInt(index), with: newMarker.position)
+                
+                
+            }
+        }
+        reDrawing(mapView: mapView)
+    }
+}
+
+// MARK:- Private functions
+
+extension GoogleMapsManager {
+    
+    // 算出該點有沒有在多邊形內
+    private func contains(polygon: [CGPoint], test: CGPoint) -> Bool {
+        if polygon.count <= 1 {
+            return false //or if first point = test -> return true
+        }
+        
+        let p = UIBezierPath()
+        let firstPoint = polygon[0] as CGPoint
+        
+        p.move(to: firstPoint)
+        
+        for index in 1...polygon.count-1 {
+            p.addLine(to: polygon[index] as CGPoint)
+        }
+        
+        p.close()
+        
+        return p.contains(test)
+    }
+    
+    private func drawTrack(mapView: GMSMapView) {
+        let line = GMSPolyline(path: GoogleMapsData.trackPath)
+        line.map = mapView
+        line.strokeColor = .blue
+        line.strokeWidth = 5
+    }
+    
+    private func drawPolygon(mapView: GMSMapView) {
+        let line = GMSPolyline(path: GoogleMapsData.polygonPath)
+        line.map = mapView
+        line.strokeColor = .orange
+        line.strokeWidth = 5
+    }
+    
+    private func reDrawing(mapView: GMSMapView) {
+        mapView.clear()
+        
+        // 重新繪製
+        for marker in GoogleMapsData.polygonMarkers {
+            let m = GMSMarker()
+            m.position    = marker.position
+            m.isDraggable = marker.isDraggable
+            m.title       = marker.title
+            m.snippet     = marker.snippet
+            m.icon        = marker.icon
+            m.map         = mapView
+        }
+        
+        drawPolygon(mapView: mapView)
+    }
+}
---
+++ googleMapsTest/ViewController.swift
@@ -0,0 +1,309 @@
+//
+//  ViewController.swift
+//  googleMapsTest
+//
+//  Created by maxkitmac on 2020/3/17.
+//  Copyright © 2020年 fredshao. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import GoogleMaps
+import GooglePlaces
+
+struct Location {
+    var latitude: CLLocationDegrees?
+    var longitude: CLLocationDegrees?
+}
+
+enum DragType: Int {
+    case dragWithFinishDrawing   = 0
+    case dragWithKeepDrawing     = 1
+    case dragNotFinishDrawingYet = 2
+}
+
+class ViewController: UIViewController {
+    
+    // MAEK: - Properties
+    
+    // 測試軌跡
+    var testTracks = [
+        Location(latitude: 24.165335, longitude: 120.661776), // 公司
+        Location(latitude: 24.164990, longitude: 120.661452),
+        Location(latitude: 24.164589, longitude: 120.660695),
+        Location(latitude: 24.164552, longitude: 120.659171),
+        Location(latitude: 24.164954, longitude: 120.657680),
+        Location(latitude: 24.166520, longitude: 120.652608),
+        Location(latitude: 24.165967, longitude: 120.652050),
+        Location(latitude: 24.166755, longitude: 120.650382),
+        Location(latitude: 24.167166, longitude: 120.650452),
+        Location(latitude: 24.168761, longitude: 120.648773)
+    ]
+    
+    var myLocationMgr: CLLocationManager!
+    
+    var path: GMSMutablePath!
+    
+    var preButtonPressed = UIButton()
+    
+    var testPointFlag = false
+    
+    var currentCooridinate: CLLocationCoordinate2D?
+    
+    var isDrag: DragType!
+    
+    let googleMgr = GoogleMapsManager.shareInstance
+    
+    // MARK: - IBOutlet
+    
+    @IBOutlet weak var mapView: GMSMapView!
+
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        updateDataSource()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        /*
+        // 調整 camera 讓 polyline 的能見度完整顯示在 MapView 上
+        var bounds: GMSCoordinateBounds = GMSCoordinateBounds()
+        for index in 0 ..< path.count() {
+            bounds = bounds.includingCoordinate(path.coordinate(at: index))
+        }
+        self.mapView.animate(with: GMSCameraUpdate.fit(bounds))
+        */
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        myLocationMgr.stopUpdatingLocation()
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func selectNumOfPolygonButtonPressed(_ sender: UIButton) {
+        
+        resetDrawingButtonPressed(UIButton())
+        
+        switch sender.tag {
+        case 3:
+            googleMgr.setNumOfPolygon(num: 3)
+            
+        case 4:
+            googleMgr.setNumOfPolygon(num: 4)
+            
+        case 5:
+            googleMgr.setNumOfPolygon(num: 5)
+            
+        case 6:
+            googleMgr.setNumOfPolygon(num: 6)
+            
+        default:
+            break
+        }
+        
+        sender.backgroundColor = .red
+        preButtonPressed = sender
+    }
+    
+    @IBAction func newTestPoint(_ sender: UIButton) {
+        googleMgr.removeTestPoint()
+        testPointFlag = true
+    }
+    
+    @IBAction func resetDrawingButtonPressed(_ sender: Any) {
+        googleMgr.setNumOfPolygon(num: 0)
+        googleMgr.resetMap(mapView: mapView)
+        preButtonPressed.backgroundColor = .orange
+        testPointFlag = false
+    }
+    
+    @IBAction func showTrackButtonPressed(_ sender: UIButton) {
+        
+        googleMgr.resetDrawingTrack()
+        
+        let locations = testTracks.map {
+            (location: Location) -> CLLocationCoordinate2D in
+            return CLLocationCoordinate2D(latitude: location.latitude!, longitude: location.longitude!)
+        }
+        googleMgr.newPoints(coordinates: locations, forTrack: mapView)
+    }
+}
+
+// MARK: - Private Functions
+
+extension ViewController {
+    private func updateDataSource() {
+        myLocationMgr = CLLocationManager()
+        myLocationMgr.delegate = self
+        myLocationMgr.distanceFilter = kCLLocationAccuracyBestForNavigation
+        myLocationMgr.desiredAccuracy = kCLLocationAccuracyBestForNavigation
+        
+        mapView.delegate = self
+    }
+    
+    private func showAlert(message: String) {
+        let alert = UIAlertController(title: "是否有在區域內？", message: message, preferredStyle: .alert)
+   
+        let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {(UIAlertAction) -> Void in
+            self.testPointFlag = false
+        })
+        
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    private func testPoint(cooridinate: CLLocationCoordinate2D) {
+        googleMgr.newTestPoint(coordinate: cooridinate, mapView: mapView)
+        testPointFlag = false
+        
+        // 檢查該點是否又在多邊形內
+        if googleMgr.checkIsInPolygon(coordinate: cooridinate) {
+            showAlert(message: "是")
+        } else {
+            showAlert(message: "否")
+        }
+    }
+}
+
+// MARK: - GMSMapViewDelegate
+
+extension ViewController: GMSMapViewDelegate {
+    
+    // 長按
+    func mapView(_ mapView: GMSMapView, didLongPressAt coordinate: CLLocationCoordinate2D) {
+        let newCoordinate = coordinate
+        currentCooridinate = newCoordinate
+        
+        print("[didLongPressAt]: \(coordinate)")
+        
+        if testPointFlag {
+            print("[Test Point] lat: \(newCoordinate.latitude), lng: \(newCoordinate.longitude)")
+            testPoint(cooridinate: newCoordinate)
+        }
+        
+        // 多邊形已畫完
+        if googleMgr.checkFinishDrawing() {
+            currentCooridinate = nil
+            isDrag = .dragWithFinishDrawing
+        }
+        // 多邊形未畫完
+        else {
+            // 拖曳
+            if isDrag == .dragNotFinishDrawingYet {
+                isDrag = .dragWithKeepDrawing
+            }
+            // 非拖曳(建立新點)
+            else {
+                print("[New Point] lat: \(newCoordinate.latitude), lng: \(newCoordinate.longitude)")
+                googleMgr.newPoint(coordinate: newCoordinate, forPolygon: mapView)
+            }
+        }
+    }
+    
+    // NOTE: 偵測「拖曳」與「長按」的function call有不同的順序, 因此另外使用DragType來判斷拖曳點的狀態
+    // 1. didBeginDragging -> didLongPressAt -> didDrag -> didEndDragging
+    // 2. didBeginDragging -> didDrag -> didEndDragging
+    
+    // 開始拖曳
+    func mapView(_ mapView: GMSMapView, didBeginDragging marker: GMSMarker) {
+        print("[didBeginDragging]: \(marker.position)")
+        
+        // 多邊形已畫完
+        if googleMgr.checkFinishDrawing() {
+            
+        }
+        // 多邊形未畫完, currentCooridinate一定有值, 再來判斷現在是拖曳哪個頂點, 然後在didDrag更新現在移動的位置
+        else {
+            if let _currentCooridinate = currentCooridinate {
+                if (_currentCooridinate.latitude == marker.position.latitude &&
+                    _currentCooridinate.longitude == marker.position.longitude) {
+                    isDrag = .dragNotFinishDrawingYet
+                }
+            }
+        }
+    }
+    
+    // 拖曳中
+    func mapView(_ mapView: GMSMapView, didDrag marker: GMSMarker) {
+        //print("[didDrag]: \(marker.position)")
+        
+        // 多邊形已畫完
+        if googleMgr.checkFinishDrawing() {
+            googleMgr.modifyPoint(newMarker: marker, mapView: mapView)
+        }
+        // 多邊形未畫完
+        else {
+            if isDrag == .dragNotFinishDrawingYet || isDrag == .dragWithKeepDrawing {
+                googleMgr.modifyPoint(newMarker: marker, whenNotFinishDrawingYet: mapView)
+            }
+        }
+    }
+    
+    // 結束拖曳
+    func mapView(_ mapView: GMSMapView, didEndDragging marker: GMSMarker) {
+        print("[didEndDragging]: \(marker.position)")
+        
+        if googleMgr.checkFinishDrawing() {
+            isDrag = .dragWithFinishDrawing
+        } else {
+            isDrag = .dragNotFinishDrawingYet
+        }
+        //mapView.reloadInputViews()
+    }
+}
+
+// MARK: - CLLocationManagerDelegate
+
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+       
+        switch status {
+        // 第一次啟用APP
+        case .notDetermined:
+            myLocationMgr.requestWhenInUseAuthorization()
+            
+        case .denied:
+            let alertController = UIAlertController(title: "定位權限已關閉", message:"如要變更權限，請至 設定 > 隱私權 > 定位服務 開啟", preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "確認", style: .default, handler:nil)
+            alertController.addAction(okAction)
+            self.present(alertController, animated: true, completion: nil)
+        
+        case .authorizedWhenInUse:
+            myLocationMgr.startUpdatingLocation()
+            mapView.isMyLocationEnabled = true
+            mapView.settings.myLocationButton = true
+            
+        default:
+            break
+        }
+    }
+    
+    // 所在位置只要有更動就會觸發
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // 印出目前所在位置座標
+        let currentLocation: CLLocation = locations[0] as CLLocation
+        print("[Current Location]: \(currentLocation.coordinate.latitude), \(currentLocation.coordinate.longitude)")
+        
+        if let location = locations.first {
+            let lat = location.coordinate.latitude
+            let lng = location.coordinate.longitude
+            let myPos = GMSCameraPosition.camera(withLatitude: lat, longitude: lng, zoom: 15)
+            mapView.animate(to: myPos)
+            
+//            let newLat = lat + 0.5
+//            let marker = GMSMarker()
+//            
+//            marker.position = CLLocationCoordinate2D(latitude: newLat, longitude: lng)
+//            marker.isDraggable = true
+//            marker.title = "TEST"
+//            marker.snippet = ""
+//            marker.map = mapView
+
+            // 避免自己位置一有變動, 畫面就強制移到定位點
+            myLocationMgr.stopUpdatingLocation()
+        }
+    }
+}
